3589e99f9c7932b576e1ccea7afaf6e809cc5def
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-12 16:55:42 +0900

    docs: fix typo

--- router.ts
+++ router.ts
@@ -100,8 +100,8 @@ function methods(
  * import { serve } from "https://deno.land/std@$VERSION/http/mod.ts";
  * const router = createRouter({
  *   "/api/students/:name": {
- *     GET: (req, { params }) => {
- *       const greeting = `Hello! ${params.name!}`;
+ *     GET: (req, ctx) => {
+ *       const greeting = `Hello! ${ctx.params.name!}`;
  *       return new Response(greeting);
  *     },
  *   },
