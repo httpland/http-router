20000a745e24fbeaee484d637feb57baeeb73b8b
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-12 19:33:47 +0900

    chore: add license to source

--- router.ts
+++ router.ts
@@ -1,3 +1,6 @@
+// Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
 import { isEmpty, isFunction, Status, STATUS_TEXT } from "./deps.ts";
 
 /** HTTP request method. */
