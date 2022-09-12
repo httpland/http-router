e70e5a6df93713f380f22e3c2b79f71289659f24
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-09-12 13:59:07 +0900

    chore: add deps

--- deps.ts
+++ deps.ts
@@ -4,12 +4,12 @@
 export {
   isFunction,
   isString,
+  isTruthy,
 } from "https://deno.land/x/isx@1.0.0-beta.21/mod.ts";
 export {
   Status,
   STATUS_TEXT,
 } from "https://deno.land/std@0.155.0/http/http_status.ts";
-export { join } from "https://deno.land/std@0.155.0/path/posix.ts";
 
 export function isEmptyObject(value: unknown): value is Record<never, never> {
   return !Object.getOwnPropertyNames(value).length &&
