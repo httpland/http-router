197304292599d9b572d24a9549f3fdd437251ab2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-09-13 22:11:14 +0900

    docs: add comment of license

--- constants.ts
+++ constants.ts
@@ -1,3 +1,6 @@
+// Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
 import { HttpMethod } from "./types.ts";
 
 /** Set of HTTP all methods. */
--- utils.ts
+++ utils.ts
@@ -1,3 +1,6 @@
+// Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
 import { isTruthy } from "./deps.ts";
 
 export function joinUrlPath(...paths: readonly string[]): string {
