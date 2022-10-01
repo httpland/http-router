d837a23ef5bb7045ab0a152adbf9298c14dc0438
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-10-02 02:52:49 +0900

    feat(mod): remove unnessesary module from API
    Remove `HttpMethods` and `HttpMethod` from the API since they are not directly related to the
    public.
    
    BREAKING CHANGE: made unimportant modules private
    
    close #18

--- mod.ts
+++ mod.ts
@@ -1,8 +1,7 @@
 // Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
 
 export { MethodRouter, nest, URLRouter } from "./routers.ts";
-export { HttpMethods } from "./constants.ts";
-export { type Handler, type HttpMethod } from "./deps.ts";
+export { type Handler } from "./deps.ts";
 export {
   type MethodRouterConstructor,
   type PathnameRoutes,
