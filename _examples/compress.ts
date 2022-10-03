14d1dae4d9885a5ace12970c1de11587c22e5c54
TomokiMiyauci <development.operation6.6.6@gmail.com>
2022-10-03 23:25:04 +0900

    docs(_examples): update example

--- _examples/compress.ts
+++ _examples/compress.ts
@@ -1,11 +1,11 @@
-import { createRouter } from "../mod.ts";
-import { serve } from "https://deno.land/std@0.156.0/http/mod.ts";
+import { MethodRouter, URLRouter } from "../mod.ts";
+import { serve } from "https://deno.land/std@0.158.0/http/mod.ts";
 import { withCompress } from "https://deno.land/x/http_compress@1.0.0/mod.ts";
 
-const handler = createRouter({
-  "/": {
+const handler = URLRouter({
+  "/": MethodRouter({
     GET: () => fetch("https://api.publicapis.org/entries"),
-  },
+  }),
 });
 
 serve(withCompress(handler));
