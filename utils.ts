33948b72799f4b3362e0b59c8e0b28ccdb2e6252
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-02-17 21:53:15 +0900

    chore: update license version

--- utils.ts
+++ utils.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-latest the httpland authors. All rights reserved. MIT license.
+// Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
 export function assert(
