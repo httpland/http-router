765a5af4eed402478db8324713aab26b2d4769df
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-02-21 02:20:28 +0900

    feat(mod): export interface and types

--- mod.ts
+++ mod.ts
@@ -1,5 +1,12 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { type MatchContext, type ParamsContext, Router } from "./router.ts";
+export {
+  type MatchContext,
+  type ParamsContext,
+  type Route,
+  type RouteContext,
+  Router,
+  type RouterLike,
+} from "./router.ts";
 export type { Handler, Middleware } from "./types.ts";
